56fce4c92249afdc7ee2a82295f8092f8a3fa9e9
ivy <livy13787@gmail.com>
2022-05-11 14:39:22 +0800

    finish buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,83 @@
+let result
+let playerScore= 0
+let computerScore=0
+const div = document.createElement('div');
+let para = document.createElement('p');
+let para2= document.createElement('p')
+let para3= document.createElement('p')
+let para4= document.createElement('p')
+let para5=document.createElement('h2')
+div.appendChild(para);
+div.appendChild(para2);
+div.appendChild(para3);
+div.appendChild(para4);
+div.appendChild(para5);
+container.appendChild(div);
+
+
+function playRound(playerSelection,computerSelection) {
+    if((playerSelection == 'rock'&&computerSelection=='rock')
+    ||(playerSelection=='paper'&&computerSelection=='paper')
+    ||(playerSelection=='scissors'&&computerSelection=='scissors' )){
+    para2.textContent=`computer says ${computerSelection}.`+" it's even"
+   }
+   
+   if((playerSelection == 'paper'&&computerSelection=='rock')
+   ||(playerSelection=='rock'&&computerSelection=='scissors')
+   ||(playerSelection=='scissors'&&computerSelection=='paper')
+
+   ) {
+       para2.textContent=`computer says ${computerSelection}.`+' you win!'
+       playerScore++;
+
+   
+   }
+   if((playerSelection == 'rock'&&computerSelection=='paper')
+   ||(playerSelection=='paper'&&computerSelection=='scissors')
+   ||(playerSelection=='scissors'&&computerSelection=='rock' )){
+   para2.textContent=`computer says ${computerSelection}.`+" you lose!"
+   computerScore++;
+  }
+ 
+
+
+para3.textContent='your score:'+playerScore
+para4.textContent="computer's score:"+computerScore
+if(playerScore==5){
+    para5.textContent='you won the game! reload to play again!'
+}
+if(computerScore==5){
+    para5.textContent='you lose! reload to play again!'
+}
+}
+
+
+
+
+    
+    
+
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click',() => {
+   
+    playRound('rock',computerPlay())
+    
+}   );
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click',() => {
+
+ para.textContent = playRound('paper',computerPlay())
+}   );
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click',() => {
+
+ para.textContent = playRound('scissors',computerPlay())
+}   );
+
+
+
 function computerPlay() {
     const play = [
         "rock",
@@ -7,36 +87,21 @@ function computerPlay() {
     const randomPlay = play[Math.floor(Math.random()*play.length)];
     return randomPlay;
 }
-let result
-function playRound(playerSelection,computerSelection) {
- if(playerSelection === 'rock') {
-     if(computerSelection === 'paper'){result='lose';return 'You lose!' }
-     if(computerSelection === 'rock') {result='even';return 'It is even!';}
-     if(computerSelection === 'scissors') {result='win';return 'You win!'}
-}
-if(playerSelection === 'paper') {
-    if(computerSelection === 'paper'){result='even';return 'It is even!'}
-    if(computerSelection === 'rock') {result='win';return 'You win!'}
-    if(computerSelection === 'scissors') {result='lose';return 'You lose!'}
 
-}
-if(playerSelection === 'scissors' ) {
-    if (computerSelection === 'paper'){result='win';return 'You win!'}
-    if(computerSelection === 'rock') {result='lose';return 'You lose!'}
-    if(computerSelection === 'scissors') {result='even';return 'It is even!'}
-}
-}
+
+
+
+
 /*const playerSelection = 'rock';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection,computerSelection));
 console.log(result);*/
 
-let playerScore = 0;
-let computerScore = 0;
+const computerSelection = computerPlay();
 function game(){
-    for(let i = 1; i <= 5; i++){
-    let playerSelection = prompt("Rock paper scissors?").toLocaleLowerCase();
-    const computerSelection = computerPlay();
+    //for(let i = 1;i<=5;i++) {
+    let playerSelection = addEventListener('mouseClick');
+    
     console.log('ROUND'+i);
     console.log('you say:'+playerSelection);
     console.log('computer says:'+computerSelection);
@@ -52,6 +117,17 @@ function game(){
     if (playerScore > computerScore){console.log("Game is over.You are the winner!")}
     if (playerScore < computerScore){console.log("Game is over.Computer is the winner!")}
     else {console.log("Game is over.It's even!")}
-    }
+    
+    
+    
 
-console.log(game())
+
+
+
+    
+
+
+    
+
+//console.log(game())
+
